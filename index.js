63e68f58a805e04cb33ffaf7108ec464b6f0b0f5
saroff03 <106846859+saroff03@users.noreply.github.com>
2022-12-19 14:18:16 +0000

    remove rate limiting

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const axios = require('axios');
 const cors = require("cors");
 const PORT = process.env.PORT || 3030;
-const rateLimit = require("express-rate-limit");
 
 
 app.use(cors());
@@ -13,15 +12,6 @@ app.use(cors());
 //Env vars
 const API_URL = process.env.API_URL
 const API_KEY = process.env.API_KEY
-
-//Rate limiting
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, //10 mins
-  max: 200,
-})
-app.use(limiter)
-app.set("trust proxy")
-
 
 app.use(bodyParser.json());
 
